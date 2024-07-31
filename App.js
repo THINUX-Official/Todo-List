8e6de0a9f66ff7dafe3b5d08952bbd5f8afa062c
THINUX-Official <thinuxofficial@gmail.com>
2024-07-31 10:28:01 +0530

    add tasks as samples.

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+
+            {/* today's tasks */}
+            <View style={styles.tasksWrapper}>
+                <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+                <View style={styles.items}>
+                    {/*    this is the where the tasks go*/}
+                    <Task text={'task 1'} />
+                    <Task text={'task 2'} />
+                </View>
+            </View>
+
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#E8EAED',
+    },
+    tasksWrapper: {
+        paddingTop: 80,
+        paddingHorizontal: 20
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    items: {
+        marginTop: 30,
+    },
 });
